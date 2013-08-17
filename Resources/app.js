7abb8830951f3844f66452272f2582a394d8b5ec
Guy Burton <guy@beatbutchermpc.com>
2013-08-17 22:20:38 +0100

    Initial commit

---
+++ Resources/app.js
@@ -0,0 +1,23 @@
+/**
+ * @author Guy Burton
+ * In app.js, we generally take care of a few things:
+ * - Bootstrap the application with any data we need
+ * - Check for dependencies like device type, platform version or network connection
+ * - Require and open our top-level UI component
+ *  
+ */
+
+//bootstrap and check dependencies
+if (Ti.version < 1.8 ) {
+	alert('Sorry - this application template requires Titanium Mobile SDK 1.8 or later');
+}
+
+(function() {
+	var osname = Ti.Platform.osname,
+		version = Ti.Platform.version,
+		height = Ti.Platform.displayCaps.platformHeight,
+		width = Ti.Platform.displayCaps.platformWidth;
+	
+	var Window = require('ui/handheld/ApplicationWindow');
+	new Window().open();
+})();
